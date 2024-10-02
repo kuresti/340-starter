3103c75de7fb0a66a72626d33833248351b0a722
kuresti <55415865+kuresti@users.noreply.github.com>
2024-10-02 11:42:42 -0600

    Update server.js
    -Wrapped baseController.buildHome with utilities.handleErrors
    -Added Express Error Handler

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("../utilities/")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -21,16 +21,33 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") //not at views root
 
-
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: "It's life Jim, but not as we know it. You are lost, take a tip from E.T. and phone Home. (click the Home button)"})
+})
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ * ***********************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
