b4e2b9173bc92afb4ec5b6fad86cb12f80871673
kuresti <55415865+kuresti@users.noreply.github.com>
2024-10-05 12:44:32 -0600

    Update server.js
    -Added Error route
    -Added route to trigger 500 error
    -Made sure file not found route is the last
    -Added logic to display 500 error page

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const errorRoute = require("./routes/errRoute")
 const utilities = require("./utilities/")
 
 /* ***********************
@@ -29,10 +30,20 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", utilities.handleErrors(inventoryRoute))
+// Error route
+app.use("/", utilities.handleErrors(errorRoute))
+// Route to cause 500 error
+app.get("/trigger-error", (req, res, next) => {
+  next({
+      status: 500,
+      message: "Oh no! There was a crash. Maybe try a different route?"
+  })
+})
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: "It's life Jim, but not as we know it. You are lost, take a tip from E.T. and phone Home. (click the Home button)"})
 })
+
 
 /* ***********************
  * Express Error Handler
@@ -44,12 +55,12 @@ app.use(async (err, req, res, next) => {
   if(err.status == 404){
     message = err.message || 'Page not found.'
     image = "./images/errors/alien-phon-home.jpg"
-  } else {
+  } else if(err.status == 500) {
     message = 'Oh no! There was a crash. Maybe try a different route?'
     image = "./images/errors/car-crash.webp"
   }
   res.render("errors/error", {
-    title: err.status || 'Server Error',
+    title: err.status || ' 500 Server Error',
     message,
     image,
     nav
