245114d40937babd1507e40e3bbdee7b77ede24d
kuresti <55415865+kuresti@users.noreply.github.com>
2024-10-11 17:42:12 -0600

    Create account-validation.js
    -created account-validation.js
    -Added validate.registrationRules
    -Added validate.checkRegData

---
+++ utilities/account-validation.js
@@ -0,0 +1,82 @@
+/* *******************************
+ * Required resources
+ * ******************************* */
+const accountModel = require("../models/account-model")
+const utilities = require(".") // require the utilities > index.js file
+    const { body, validationResult } = require("express-validator") // require the express validator. This line contains two tools: body (allows validator to access body obj) and validationResult (contains all errors detected by validation process)
+    const validate = {} // create a "validate" object
+
+/* *******************************
+ * Registration Data Validation Rules
+ * ******************************* */
+validate.registrationRules = () => { // anonymous function assigned to "registrationRules" property of the "validate" obj.
+    return [ // "return" command and opening of array of checks for incoming data
+        // firstname is required and must be a string
+        body("account_firstname") //looks inside HTTPRequest for name-value pair, "account_firstname".
+            .trim() // sanitizing function that removes whitespace at the beginning and end of incoming string
+            .escape() // finds any special charachter and transforms it to an HTML entity rendering it not operational as code.
+            .notEmpty() // validator ensuring that a value exists.
+            .isLength({ min: 1 }) // validator checking for specified length requirement.
+            .withMessage("Please provide a first name."), // On error message is sent
+
+        // lastname is required and must be a string
+        body("account_lastname") // Rule for lastname that is similar to rule for firstname
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 2 })
+            .withMessage("Please provide a last name."), // On error message is sent
+
+        // valid email is required and cannot already exist in the DB
+        body("account_email") // looks inside HTTPRequest for name-value pair, "account-email"
+            .trim()
+            .escape()
+            .notEmpty()
+            .isEmail() // function that checks the string for characters that should be present in a valid email address.
+            .normalizeEmail()  // sanitization function that makes all email lowercse, as well as additional alterations to "canonicalize" an email.
+            .withMessage("A valid email is required.") // On error message is sent
+            .custom(async (account_email) => { //creates "custom" check. In custom check an async arrow function is created and the "account_email" variable is a parameter
+                const emailExists = await accountModel.checkExistingEmail(account_email) // calling the function from the model and collecting the value returned (should be 0 or 1)
+                if (emailExists){ // "if" control structure to check the result. Remember that "0" is FALSE, while any other value is TRUE.
+                    throw new Error("Email exists. Please log in or use a different email") // throws an error and an error mssg indicating email exists and can't be reused if row count is 1.
+                }
+            }),
+
+        //password is required and must be a strong password
+        body("account_password") // begins password check process
+            .trim()
+            .notEmpty()
+            .isStrongPassword({ // function for checking a passowrd string to meet specific requirements to be considered a strong password. Returns boolean term. Optionally can return a strength score.
+                minLength: 12, //name-value pair that indicates the min length of the password
+                minLowercase: 1, // name-value pair that indicates the min number of lowercase alphabetic characters
+                minUppercase: 1, // name-value pair that indicates the min number of Uppercase alphabetic characters
+                minNumbers: 1, // name-value pair that indicates the min number of numeric characters
+                minSymbols: 1, // name-value pair that indicates the min number of symbol characters
+            })
+            .withMessage("Password does not meet requirements."),
+    ]
+}
+
+/* *********************************
+ * Check data and return errors or continue to registration
+ * ********************************* */
+validate.checkRegData = async (req, res, next) => { // creates async, anonymous function accepts(req, res, next) as params, assigns it to the "checkRegData" property of the validate object.
+    const { account_firstname, account_lastname, account_email, } = req.body //uses JS destructuring method to collect and store firstname, lastname, adn email values from the request body.
+    let errors = [] // creates an empty "errors" array
+    errors = validationResult(req) // calls the express-validator "validationResult" function and sends the request obj that contains all incoming data as a parameter. If there are errors they are stored in the errors array.
+    if (!errors.isEmpty()) { // checks the errors array to see if any errors exist. 
+        let nav = await utilities.getNav() //calls for the nav bar to be queried and built.
+        res.render("account/registration", { // calls the render function to rebuild the registration view.
+            errors, // this and items below are sent back to the view.
+            title: "Registration",
+            nav,
+            account_firstname,
+            account_lastname,
+            account_email
+        })
+        return // the "return" command sends control of the process back to the application, so the view in the browser does not "hang".
+    }
+    next() // if no errors are detected, the "next()" function is called, which allows the process to continue into the controller for the registration to be carried out.
+}
+
+module.exports = validate
