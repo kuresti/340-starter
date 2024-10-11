0c400705b870fddb02048bd63d5b9f5c96dffce7
kuresti <55415865+kuresti@users.noreply.github.com>
2024-10-11 17:39:41 -0600

    Update accountRoute.js
    -Added regValidate
    -Added router to deliver registration view
    -Added router to process registration
    -Added router to post a login attempt

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,14 +2,42 @@
  * Needed Resources
  * Account routes
  * ***************************** */
+const regValidate = require("../utilities/account-validation")
 const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/acctController")
-const utilities = require("../utilities/")
+const utilities = require("../utilities")
+
 
 /* *****************************
  * Deliver Login View
  * ***************************** */
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
+/* ****************************
+ * Deliver registration view
+ * **************************** */
+router.get("/registration", utilities.handleErrors(accountController.buildRegistration))
+
+/* ****************************
+ * Process Registration
+ * **************************** */
+// Process the reigstration data
+router.post( // Router object using a "post" property.
+    "/registration", // Path being watched for in the route.
+    regValidate.registrationRules(), // The function containing the rules to be used in the validation process.
+    regValidate.checkRegData, // The call to run the validation and handle the errors, if any.
+    utilities.handleErrors(accountController.registerAccount) // The call to the controller to handle the registration, if no errors occur in the validation process.
+)
+
+// Process the login attempt
+router.post( // has the router listening for a "post" request.
+    "/login", // indicates the route being watched for inside the post request.
+    (req, res) => { //an arrow function that accepts req, res obj as params
+        res.status(200).send('login process') //indicates that the req was successful and returns "login process"
+    }
+)
+
 module.exports = router
+
+
