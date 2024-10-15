7e2ecda537fa9bbf8bc52e18e635303b04bb4c2f
kuresti <55415865+kuresti@users.noreply.github.com>
2024-10-14 20:19:20 -0600

    Update inventoryRoute.js
    -Added get route for /management
    -Added get route for /add-classification
    -Added get route for /add-inventory
    -Added post route for /add-classification
    -Added post route for /add-inventory

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,13 +1,39 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
-const invController = require("../controllers/invController")
-const utilities = require("../utilities/")
+const invCont = require("../controllers/invController");
+const utilities = require("../utilities")
+const invValidate = require("../utilities/inventory-validation")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+router.get("/type/:classificationId", utilities.handleErrors(invCont.buildByClassificationId));
 
 // Route to build inventory item by invId view
-router.get("/details/:invId", utilities.handleErrors(invController.buildByInvId));
+router.get("/details/:invId", utilities.handleErrors(invCont.buildByInvId));
+
+// Route to build inventory management view
+router.get("/management", utilities.handleErrors(invCont.buildInvManagement));
+
+// Route to build add-classification view
+router.get("/add-classification", utilities.handleErrors(invCont.buildAddClassification));
+
+// Route to build add-inventory view
+router.get("/add-inventory", utilities.handleErrors(invCont.buildAddInventory))
+
+// Insert new classification to the DB
+router.post(
+    "/add-classification",
+    invValidate.addClassificationRules(),
+    invValidate.checkClassificationNameData,
+    utilities.handleErrors(invCont.insertClassificationName)
+)
+
+// Insert add vehicle to the DB
+router.post(
+    "/add-inventory",
+    invValidate.addInventoryRules(),
+    invValidate.checkAddInventoryData,
+    utilities.handleErrors(invCont.insertAddInventory)
+)
 
 module.exports = router;
