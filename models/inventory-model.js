f22af4a86cf6db0d0b5ebc851678d65fd283b818
kuresti <55415865+kuresti@users.noreply.github.com>
2024-10-21 15:43:00 -0600

    Update inventory-model.js
    -Added deleteInventory query function

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -105,6 +105,20 @@ async function getInventoryByClassificationId(classification_id) {
     }   
   }
 
+  /* ***************************
+   * Delete Inventory Data
+   * *************************** */
+  async function deleteInventory(inv_id) {
+   
+    try {
+      const sql = "DELETE FROM inventory WHERE inv_id = $1"        
+      const data = await pool.query(sql, [ inv_id ])
+      return data
+    } catch (error) {
+      new Error("Delete Inventory Error")
+    }   
+  }
+
   
 
-  module.exports = {getClassifications, getInventoryByClassificationId, getInvItemByInvId, postClassificationName, postAddInventory, updateInventory};
+  module.exports = {getClassifications, getInventoryByClassificationId, getInvItemByInvId, postClassificationName, postAddInventory, updateInventory, deleteInventory};
